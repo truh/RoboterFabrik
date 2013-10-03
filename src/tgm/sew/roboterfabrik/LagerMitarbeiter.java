1b9eeafddcf4e0605b63a031691404499948714a
Jakob Klepp <jakob@truh.in>
2013-10-03 21:21:27 +0200

    LagerMitarbeiter

--- src/tgm/sew/roboterfabrik/LagerMitarbeiter.java
+++ src/tgm/sew/roboterfabrik/LagerMitarbeiter.java
@@ -1,26 +1,78 @@
 package tgm.sew.roboterfabrik;
+
+import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class LagerMitarbeiter implements Mitarbeiter {
 
 	private Lager lager;
 
-	private Lieferant lieferant;
-
-	private SpielzeugRoboter spielzeugRoboter;
-
 	private int id;
 
-	public Stringifyable anfrage(Class<? extends Stringifyable> type) {
-		return null;
+    private ArrayBlockingQueue <Stringifyable> einlagernQueue;
+
+    private boolean stop;
+    /**
+     * Erstellt einen LagerMitarbeiter welche an gegebenen filePfad ein Lager eröffnet
+     *
+     * @param filePfad Ort an dem gelagert werden soll
+     */
+    public LagerMitarbeiter(int id, String filePfad)
+    {
+        stop = false;
+        this.id = id;
+        try
+        {
+            this.lager = new Lager(filePfad);
+        } catch (IOException e)
+        {
+            e.printStackTrace();  //ToDo Logger
+        }
+        einlagernQueue = new ArrayBlockingQueue<Stringifyable>(50);
+    }
+
+    /**
+     * Gibt den LagerMitarbeiter die Anweisung im Lager nach einen bestimmten Artikel zu suchen
+     *
+     * @param type Typ des Artikels
+     * @return Artikel oder null
+     */
+    public Stringifyable anfrage(Class<? extends Stringifyable> type) {
+        Stringifyable item = null;
+
+        if(type == Arm.class)
+        {
+            item = lager.pollArm();
+        } else
+        if(type == Auge.class)
+        {
+            item = lager.pollAuge();
+        } else
+        if(type == Kettenantrieb.class)
+        {
+            item = lager.pollKettenantrieb();
+        } else
+        if(type == Rumpf.class)
+        {
+            item = lager.pollRumpf();
+        } else
+        if(type == SpielzeugRoboter.class)
+        {
+            item = lager.pollThreadee();
+        }
+		return item;
 	}
 
+    /**
+     * Ein Artikel wird dem Lagermitarbeiter zurückgeliefert
+     * Arbeitet asynchron, item wird erst zwischen gespeichert und später eingelagert
+     *
+     * @param item Artikel der eingeliefert werden soll
+     */
 	public void einlagern(Stringifyable item) {
-
+        this.einlagernQueue.add(item);
 	}
 
-	public LagerMitarbeiter(String filePfad) {
-
-	}
-	
 	/**
 	 * @see Stoppable#stop()
 	 */
@@ -28,14 +80,47 @@ public class LagerMitarbeiter implements Mitarbeiter {
 
 	}
 
+    /**
+     * @see Runnable#run()
+     */
 	public void run()
 	{
-		
+	    while (!stop)
+        {
+            if(einlagernQueue.size() > 0)
+            {
+                Stringifyable item = einlagernQueue.poll();
+                if(item instanceof Arm)
+                {
+                    lager.addArm((Arm)item);
+                } else
+                if(item instanceof Auge)
+                {
+                    lager.addAuge((Auge) item);
+                } else
+                if(item instanceof Kettenantrieb)
+                {
+                    lager.addKettenantrieb((Kettenantrieb) item);
+                } else
+                if(item instanceof Rumpf)
+                {
+                    lager.addRumpf((Rumpf) item);
+                } else
+                if(item instanceof SpielzeugRoboter)
+                {
+                    lager.addThreadee((SpielzeugRoboter) item);
+                }
+            }
+        }
 	}
 
+    /**
+     * Für was aucher immer der LagerMitarbeiter die id brauchen soll
+     * @return eindeutige ID
+     */
     @Override
     public int getId() {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        return this.id;
     }
 
     @Override
