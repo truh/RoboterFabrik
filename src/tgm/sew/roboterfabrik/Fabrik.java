9e8194e49fc5a758e944556b38c244c10b86abdd
Andreas Vogt <andi6444@hotmail.com>
2013-10-03 21:44:43 +0200

    Test

--- src/tgm/sew/roboterfabrik/Fabrik.java
+++ src/tgm/sew/roboterfabrik/Fabrik.java
@@ -13,7 +13,7 @@ public class Fabrik {
 	private ThreadPoolExecutor montageMitarbeiterPool;
 	
 	private ThreadPoolExecutor lagerMitarbeiterPool;
-
+	
 	private ThreadPoolExecutor kundenPool;
 
 	private ThreadPoolExecutor liferantenPool;
